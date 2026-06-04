c0a03c1611d4cd6c77d866f0ce3f0b6549bdbe68
L3id4n <dani03ghetto@gmail.com>
2026-06-04 10:16:56 -0300

    Alterações na mensagem de resultado, divisão por 0 não é mais possível, opção de operação não reseta mais depois do resultado e limpa a caixa de valores, remoção do texto de valores utilizados.

--- app.py
+++ app.py
@@ -8,29 +8,46 @@ def inicio():
     
     number1 = 0
     number2 = 0
-    result = 0
+    result = None
     operacao = ""
+    mensagem = ""
+    mensagem_resultado = ""
     
     if request.method == "POST":
         operacao = request.form["operacao"]
         number1 = request.form["number1"]
         number2 = request.form["number2"]
+        
         if operacao == "soma":
             result = float(number1) + float(number2)
+            mensagem_resultado = f"O resultado da soma é {result}"
         elif operacao == "subtracao":
             result = float(number1) - float(number2)
+            mensagem_resultado = f"O resultado da subtração é {result}"
         elif operacao == "multiplicacao":
             result = float(number1) * float(number2)
+            mensagem_resultado = f"O resultado da multiplicação é {result}"
         elif operacao == "divisao":
-            result = float(number1) / float(number2)
+            if float(number2) == 0:
+                mensagem = "Não é possível dividir por zero."
+                mensagem_resultado = ""
+            else:
+                result = float(number1) / float(number2)
+                mensagem_resultado = f"O resultado da divisão é {result}"
         elif operacao == "potencia":
             result = float(number1) ** float(number2)
+            mensagem_resultado = f"O resultado da potenciação é {result}"
+            
+        number1 = ""
+        number2 = ""
     
     return render_template(
         "index.html",
         number1=number1,
         number2=number2,
         result=result,
+        mensagem=mensagem,
+        mensagem_resultado=mensagem_resultado,
         operacao=operacao
     )
     
